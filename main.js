f7fc6cc66e5bb9e3a3b4fac650718d7cbb001690
dev-rohit0 <93703830+dev-rohit0@users.noreply.github.com>
2021-12-04 21:37:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
+//created using Ajax and Fetch API you can use any of two method
+
 let fact = document.querySelector('#fact');
 let factText = document.querySelector('#factText');
 
  let numberInput = document.querySelector('#numberInput');
-
+// numberInput.addEventListener('input',getFact);
  numberInput.addEventListener('input',getFactFetch);
 
+// function getFactAjax(){
+//     let number = numberInput.value;
+//     let xhr = new XMLHttpRequest();
+//     xhr.open('GET','http://numbersapi.com/'+number);
+
+//     xhr.onload = function(){
+//         if (this.status == 200 && number != '') {
+//             fact.style.display = 'block';
+//             factText.innerHTML = this.responseText;
+//         }if(this.status == 200 && number == '') {
+//             fact.style.display = 'none';
+//         }
+
+//     }
+//     xhr.send();
+
+   
+// }
 
 function getFactFetch(){
     let number = numberInput.value;
