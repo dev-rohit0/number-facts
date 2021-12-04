0f6ad96e91cfd649d2b64917126ab8c3fb6dba23
dev-rohit0 <93703830+dev-rohit0@users.noreply.github.com>
2021-12-04 21:30:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,27 +2,9 @@ let fact = document.querySelector('#fact');
 let factText = document.querySelector('#factText');
 
  let numberInput = document.querySelector('#numberInput');
-// numberInput.addEventListener('input',getFact);
+
  numberInput.addEventListener('input',getFactFetch);
 
-// function getFactAjax(){
-//     let number = numberInput.value;
-//     let xhr = new XMLHttpRequest();
-//     xhr.open('GET','http://numbersapi.com/'+number);
-
-//     xhr.onload = function(){
-//         if (this.status == 200 && number != '') {
-//             fact.style.display = 'block';
-//             factText.innerHTML = this.responseText;
-//         }if(this.status == 200 && number == '') {
-//             fact.style.display = 'none';
-//         }
-
-//     }
-//     xhr.send();
-
-   
-// }
 
 function getFactFetch(){
     let number = numberInput.value;
